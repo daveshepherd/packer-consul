bcd457a9b5ccbe0b79070d011f8878e8cc6cea0a
Dave Shepherd <david.shepherd@wealthwizards.com>
2018-03-12 21:36:14 +0000

    Migrate to use ciricleci

--- build.sh
+++ build.sh
@@ -6,6 +6,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PACKER=$(which packer)
 [[ -z ${PACKER} ]] &&  echo 'packer command not found in the search path. exiting...' && exit 1
 
-source ${DIR}/../environments/config.env
-
 ${PACKER} build -var "london_vpc_id=${LONDON_VPC}" -var "london_subnet_id=${LONDON_SUBNET}" -var "ireland_vpc_id=${IRELAND_VPC}" -var "ireland_subnet_id=${IRELAND_SUBNET}" -var "consul_version=1.0.6" consul.json

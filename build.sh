bd788d274abe5e27d0e974c759514d24a42ef469
Dave Shepherd <david.shepherd@wealthwizards.com>
2018-03-18 15:32:08 +0000

    Clean up the build script

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 #! /bin/bash
 
-# Ensure we use the containing directory, irrelevant of where this script is called from
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
 PACKER=$(which packer)
 [[ -z ${PACKER} ]] &&  echo 'packer command not found in the search path. exiting...' && exit 1
 
-${PACKER} build -var "london_vpc_id=${LONDON_VPC}" -var "london_subnet_id=${LONDON_SUBNET}" -var "ireland_vpc_id=${IRELAND_VPC}" -var "ireland_subnet_id=${IRELAND_SUBNET}" -var "consul_version=1.0.6" consul.json
+CONSUL_VERSION=1.0.6
+
+${PACKER} build -var "london_vpc_id=${LONDON_VPC}" -var "london_subnet_id=${LONDON_SUBNET}" -var "ireland_vpc_id=${IRELAND_VPC}" -var "ireland_subnet_id=${IRELAND_SUBNET}" -var "consul_version=${CONSUL_VERSION}" consul.json

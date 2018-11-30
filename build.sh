2834ad43e6225f9e61a1aa7329969a052ad65abf
Dave Shepherd <david.shepherd@wealthwizards.com>
2018-11-30 16:07:16 +0000

    Update to consul 1.2.4

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 PACKER=$(which packer)
 [[ -z ${PACKER} ]] &&  echo 'packer command not found in the search path. exiting...' && exit 1
 
-CONSUL_VERSION=1.2.2
+CONSUL_VERSION=1.2.4
 
 ${PACKER} build -var "region=${REGION}" -var "vpc_id=${VPC_ID}" -var "subnet_id=${SUBNET_ID}" -var "consul_version=${CONSUL_VERSION}" -var "destination_regions=${DESTINATION_REGIONS}" build.json

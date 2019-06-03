9d6087fd9567b58fec6d1dbca5d96104c3318c03
dave-shepherd <david.shepherd@wealthwizards.com>
2019-06-03 14:55:29 +0100

    Update consul to 1.5.1

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 PACKER=$(which packer)
 [[ -z ${PACKER} ]] &&  echo 'packer command not found in the search path. exiting...' && exit 1
 
-CONSUL_VERSION=1.4.5
+CONSUL_VERSION=1.5.1
 
 ${PACKER} build -var "region=${REGION}" -var "vpc_id=${VPC_ID}" -var "subnet_id=${SUBNET_ID}" -var "consul_version=${CONSUL_VERSION}" -var "destination_regions=${DESTINATION_REGIONS}" build.json
